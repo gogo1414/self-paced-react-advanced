6efbff7e58a1a364ba9188f9056dba916c9ceae1
gogo1414 <wnstn6945@gmail.com>
2024-11-20 14:31:42 +0900

    refactor: CategoryFilter styled 적용 및 기존 css 제거

--- src/components/Main/CategoryFilter.jsx
+++ src/components/Main/CategoryFilter.jsx
@@ -1,15 +1,31 @@
 import Categories from "../../data/Category.js";
-import styles from "../../css/CategoryFilter.module.css"; 
+import styled from "styled-components";
+
+const RESTAURANT_FILTER_CONTAINER = styled.section`
+    display: flex;
+    justify-content: space-between;
+    padding: 0 16px;
+    margin-top: 24px;
+    
+    &select {
+        height: 44px;
+        min-width: 125px;
+        border: 1px solid #d0d5dd;
+        border-radius: 8px;
+        background: transparent;
+        font-size: 16px;
+        padding: 8px;
+    }
+`;
 
 function CategoryFilter({ category, onChangeCategory }) {
     const categories = Categories();
 
     return (
-        <section className={styles['restaurant-filter-container']}>
+        <RESTAURANT_FILTER_CONTAINER>
             <select 
                 name="category" 
-                id="category-filter" 
-                className={styles["restaurant-filter"]} 
+                id="category-filter"
                 aria-label="음식점 카테고리 필터"
                 value={category}
                 onChange={(event) => onChangeCategory(event.target.value)}
@@ -18,7 +34,7 @@ function CategoryFilter({ category, onChangeCategory }) {
                     <option key = {category.id} value={category.name}>{category.name}</option>
                 ))}
             </select>
-        </section>
+        </RESTAURANT_FILTER_CONTAINER>
     );
 }
 
