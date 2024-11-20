b1b780f44ebd1f24704e63605d1fd9c62b1e674b
gogo1414 <wnstn6945@gmail.com>
2024-11-20 14:27:36 +0900

    refactor: Header 컴포넌트 styled 적용

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,44 @@
 import styles from "../../css/Header.module.css";
+import styled from "styled-components";
+
+const GNB = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 64px;
+    padding: 0 16px;
+    background-color: var(--primary-color);
+`;
+
+const GNB_TITLE = styled.h1`
+    color: #fcfcfd;
+    font-size: 24px;
+`;
+
+const GNB_BUTTON = styled.button`
+    height: 40px;
+    border: none;
+    border-radius: 8px;
+    background: transparent;
+    font-size: 24px;
+    cursor: pointer;
+    
+    &img {
+        display: block;
+        width: 40px;
+        height: 40px;
+        object-fit: contain;
+    }
+`;
 
 function Header({ onChangeAddModal }) {
     return (
-        <header className={styles.gnb}>
-            <h1 className={`${styles.gnb__title} text-title`}>점심 뭐 먹지</h1>
-            <button type="button" className={styles.gnb__button} aria-label="음식점 추가" onClick={onChangeAddModal}>
+        <GNB>
+            <GNB_TITLE>점심 뭐 먹지</GNB_TITLE>
+            <GNB_BUTTON type="button" aria-label="음식점 추가" onClick={onChangeAddModal}>
                 <img src="../templates/add-button.png" alt="음식점 추가"></img>
-            </button>
-        </header>
+            </GNB_BUTTON>
+        </GNB>
     );
 }
 
