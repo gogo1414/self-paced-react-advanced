fd6dd248e2b515a6399e0ed784eb7bb2f60eab76
gogo1414 <wnstn6945@gmail.com>
2024-11-20 15:59:16 +0900

    refactor: RestaurantItem styled 적용

--- src/components/Main/RestaurantItem.jsx
+++ src/components/Main/RestaurantItem.jsx
@@ -1,16 +1,68 @@
 import styles from '../../css/RestaurantList.module.css';
+import styled from "styled-components";
+
+const RESTAURANT_WRAPPER = styled.li`
+    display: flex;
+    align-items: flex-start;
+    padding: 16px 8px;
+    border-bottom: 1px solid #e9eaed;
+`;
+
+const RESTAURANT_CATEGORY = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 64px;
+    height: 64px;
+    min-width: 64px;
+    min-height: 64px;
+    margin-right: 16px;
+    border-radius: 50%;
+    background: var(--lighten-color);
+`;
+
+const CATEGORY_ICON = styled.img`
+    width: 36px;
+    height: 36px;
+`;
+
+const RESTAURANT_INFO = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+`;
+
+const RESTAURANT_NAME = styled.h3`
+    margin: 0;
+    font-size: 18px;
+    line-height: 28px;
+    font-weight: 600;
+`;
+
+const RESTAURANT_DESCRIPTION = styled.p`
+    display: -webkit-box;
+    padding-top: 8px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 400;
+`;
 
 function RestaurantItem({ name, description, category, alt, onClick }) {
     return (
-        <li className={styles['restaurant']} onClick={onClick}>
-          <div className={styles['restaurant__category']}>
-            <img src={convertCategoryToImageSrc(category)} alt={alt} className={styles["category-icon"]}></img>
-          </div>
-          <div className={styles['restaurant__info']}>
-            <h3 className={`${styles.restaurant__name} text-subtitle`}>{name}</h3>
-            <p className={`${styles.restaurant__description} text-body`}>{description}</p>
-          </div>
-        </li>
+        <RESTAURANT_WRAPPER onClick={onClick}>
+          <RESTAURANT_CATEGORY>
+            <CATEGORY_ICON src={convertCategoryToImageSrc(category)} alt={alt}></CATEGORY_ICON>
+          </RESTAURANT_CATEGORY>
+          <RESTAURANT_INFO>
+            <RESTAURANT_NAME>{name}</RESTAURANT_NAME>
+            <RESTAURANT_DESCRIPTION>{description}</RESTAURANT_DESCRIPTION>
+          </RESTAURANT_INFO>
+        </RESTAURANT_WRAPPER>
     );
 }
 
